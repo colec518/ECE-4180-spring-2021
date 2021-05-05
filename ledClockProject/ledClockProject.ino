d38c5c73dedb7c5db546db63c3a41980f0835f45
cole-ifthisusernameistakenimgoingtocry <colec518@gmail.com>
2021-05-05 00:54:47 -0400

    some minor changes to allow the auto time setting to be used
    it only seems to work about half the time, but when it works, the time is set successfully. most likely a wifi related problem that could be fixed

--- ledClockProject/ledClockProject.ino
+++ ledClockProject/ledClockProject.ino
@@ -24,7 +24,7 @@ bool daylightSavingsEnabled=true;
 #define COLOR_DEPTH 24                  // Choose the color depth used for storing pixels in the layers: 24 or 48 (24 is good for most sketches - If the sketch uses type `rgb24` directly, COLOR_DEPTH must be 24)
 const uint16_t kMatrixWidth = 32;       // Set to the width of your display, must be a multiple of 8
 const uint16_t kMatrixHeight = 16;      // Set to the height of your display
-const uint8_t kRefreshDepth = 36;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
+const uint8_t kRefreshDepth = 24;       // Tradeoff of color quality vs refresh rate, max brightness, and RAM usage.  36 is typically good, drop down to 24 if you need to.  On Teensy, multiples of 3, up to 48: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48.  On ESP32: 24, 36, 48
 const uint8_t kDmaBufferRows = 4;       // known working: 2-4, use 2 to save RAM, more to keep from dropping frames and automatically lowering refresh rate.  (This isn't used on ESP32, leave as default)
 const uint8_t kPanelType = SM_PANELTYPE_HUB75_16ROW_MOD8SCAN;   // Choose the configuration that matches your panels.  See more details in MatrixCommonHub75.h and the docs: https://github.com/pixelmatix/SmartMatrix/wiki
 const uint32_t kMatrixOptions = (SM_HUB75_OPTIONS_NONE);        // see docs for options: https://github.com/pixelmatix/SmartMatrix/wiki
@@ -39,7 +39,7 @@ SMARTMATRIX_ALLOCATE_INDEXED_LAYER(indexedLayer, kMatrixWidth, kMatrixHeight, CO
 
 const int defaultBrightness = 100;
 const int defaultScrollOffset = 6;
-const rgb24 defaultBackgroundColor = {0x40, 0, 0};
+const rgb24 defaultBackgroundColor = {0x40, 0, 0};     
 const rgb24 sunlightColor = {0xff, 0xca, 0x7c};
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -120,7 +120,7 @@ int beepTime = 0;
 //WIFI/BLUETOOTH VARIABLES
 ////////////////////////////////////////////////////////////////////////////////
 
-#define DEBUG false
+#define DEBUG true
 
 BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
@@ -211,16 +211,20 @@ void loop() {
       break;
     case CONNECT_BLUETOOTH:
       drawText("connect","Bluetooth");
+      handleButton(10);
       break;
     case GET_WIFI_INFO:
       if(bluetoothMode==2)drawText("WiFi","Password");
       else drawText("WiFi","SSID");
+      handleButton(10);
       break;
     case SET_TIMEZONE:
       drawTimeZoneEdit();
+      handleButton(10);
       break;
     case SET_DAYLIGHT:
       drawDaylightEdit();
+      handleButton(10);
   }
   updateButtons();
   bluetoothConnectionCheck();
@@ -255,7 +259,7 @@ void updateButtons() {
 
 //take action based on which button has been pressed
 void handleButton(int bNum) {
-  startBeep();
+  if(bNum < 10) startBeep();
   switch (programState) {
     case DISPLAY_TIME:
       if (bNum == 3) {
@@ -305,6 +309,7 @@ void handleButton(int bNum) {
               break;
             case M_SET_AUTO:
               bluetoothMode=1;
+              programState = CONNECT_BLUETOOTH;
               //TODO: more stuff need to go here
               break;
           }
